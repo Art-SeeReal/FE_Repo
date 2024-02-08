e03be355ce63f66a22ebc936168c35279c1488be
laon2019 <laon5987@nate.com>
2024-02-08 21:11:19 +0900

    refactor : 코드 수정

--- src/hooks/userQueries.tsx
+++ src/hooks/userQueries.tsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import api from '../utils/api';
 import { UserData } from '../model/UserTypes';
 
-const fetchLogIn =  async ({ id, pw }: UserData): Promise<UserData> => {
+const fetchLogIn = async ({ id, pw }: UserData): Promise<UserData> => {
   console.log(id, pw);
   const response = await api.get('/login', { params: { id, pw } });
   console.log(response.data);
