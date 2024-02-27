fab695bcd4833d88de5abe7ef5b0689da92d05b4
이소담 <daisydam@adictives.com>
2024-02-27 18:54:32 +0900

    feat: util 쿼리, api 추가

---
+++ src/hooks/useUtilQuery.ts
@@ -0,0 +1,17 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { getAreas } from '../api/utils';
+
+const QUERY_KEY = {
+  areas: 'areas',
+} as const;
+
+interface ResponseData {
+  results: { code: string; label: string }[];
+}
+
+export const useFetchAreas: () => UseQueryResult<ResponseData> = () => {
+  return useQuery({
+    queryKey: [QUERY_KEY.areas],
+    queryFn: getAreas,
+  });
+};
