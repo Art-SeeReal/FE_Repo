3271d65d4dffb7a1c72fa19b467bc5e0881b44ab
이소담 <daisydam@adictives.com>
2024-03-14 17:08:25 +0900

    refactor: api interceptor 리액트 엘리먼트로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { DialogContainer } from './hooks/useDialogState';
 import ToastList from './components/ToastList';
 import RegisterArtistPage from './pages/ArtistPage/RegisterPage';
 import ArtistPage from './pages/ArtistPage';
+import { Interceptor } from './utils/api';
 
 const queryClient = new QueryClient();
 
@@ -23,27 +24,29 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/join" element={<SignupPage />} />
-            <Route path="/agree" element={<ConsentPage />} />
-            <Route path="/find-id-pw" element={<FindIdPwPage />} />
-            <Route path="/artist">
-              <Route index element={<ArtistPage />} />
-              <Route path="register" element={<RegisterArtistPage />} />
+        <Interceptor>
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/join" element={<SignupPage />} />
+              <Route path="/agree" element={<ConsentPage />} />
+              <Route path="/find-id-pw" element={<FindIdPwPage />} />
+              <Route path="/artist">
+                <Route index element={<ArtistPage />} />
+                <Route path="register" element={<RegisterArtistPage />} />
+              </Route>
+              <Route path="/test">
+                <Route index element={<TestPage />} />
+                <Route path="private" element={<PrivatePage />} />
+              </Route>
+              <Route path="*" element={<NotFoundPage />} />
             </Route>
-            <Route path="/test">
-              <Route index element={<TestPage />} />
-              <Route path="private" element={<PrivatePage />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
+          </Routes>
 
-        <DialogContainer />
-        <ToastList />
+          <DialogContainer />
+          <ToastList />
+        </Interceptor>
       </RecoilRoot>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
---
+++ src/utils/api.tsx
@@ -0,0 +1,39 @@
+import axios, { AxiosError, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
+import React, { ReactNode, useEffect } from 'react';
+import { useToast } from '../hooks/useToastState';
+
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '',
+  headers: { 'Content-Type': 'application/json' },
+});
+
+const Interceptor = ({ children }: { children: ReactNode }) => {
+  const { appendToast } = useToast();
+
+  useEffect(() => {
+    api.interceptors.request.use(
+      (config: InternalAxiosRequestConfig) => {
+        return config;
+      },
+      (error: Promise<AxiosError>) => {
+        return Promise.reject(error);
+      },
+    );
+
+    api.interceptors.response.use(
+      (response: AxiosResponse) => {
+        return response;
+      },
+      (error: AxiosError<{ message: string }>) => {
+        appendToast({ content: error.response?.data.message || error.message, type: 'error' });
+        return Promise.reject(error);
+      },
+    );
+  }, []);
+
+  return <div>{children}</div>;
+};
+
+export default api;
+
+export { Interceptor };
