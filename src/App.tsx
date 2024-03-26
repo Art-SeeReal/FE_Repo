d9b2223d15c2ffe315d579a72f9e88fb719f3674
laon2019 <laon5987@nate.com>
2024-03-26 23:54:55 +0900

    refactor : 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { DialogContainer } from './hooks/customs/useDialogState';
 import ToastList from './components/ToastList';
 import { Interceptor } from './utils/api';
 import Router from './router';
+import ScrollToTop from './components/ScrollTop';
 
 const App = () => {
   return (
     <RecoilRoot>
       <Interceptor>
+        <ScrollToTop />
         <Router />
         <DialogContainer />
         <ToastList />
