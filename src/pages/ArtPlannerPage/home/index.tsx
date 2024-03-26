94628636a99128934e8b4541d28466e943b6561b
이소담 <daisydam@adictives.com>
2024-03-26 17:57:45 +0900

    feat: 기획자 마이페이지 임시 추가 (개발 완료안됨, 추후 수정 필요)

---
+++ src/pages/ArtPlannerPage/home/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+// import * as S from '../../../components/styles';
+// import { useUserProfile } from '../../../hooks/customs/useUser';
+// import UserIntro from '../../../components/user/UserIntro';
+
+// const ArtPlannerHomePage = () => {
+//   const { profile } = useUserProfile();
+//   // const {
+//   //   data: profile,
+//   //   isSuccess,
+//   //   //  refetch
+//   // } = useFetchUserProfile(Number(params.id));
+//   // const setProfile = useSetRecoilState(profileState);
+
+//   // useEffect(() => {
+//   //   if (isSuccess) {
+//   //     setProfile(profile);
+//   //   }
+//   // }, [isSuccess]);
+
+//   // useEffect(() => {
+//   //   console.log(profile);
+//   // }, [profile]);
+
+//   // const onSuccess = () => refetch();
+
+//   return (
+//     <>
+//       <S.Title>{profile?.nickname}</S.Title>
+//       {profile?.phone && <p>연락처 : {profile?.phone}</p>}
+//       {profile?.email && <p>이메일 : {profile?.email}</p>}
+//       <UserIntro />
+//       {/* <UserIntroForm intro={profile?.intro} onSuccess={onSuccess} /> */}
+//     </>
+//   );
+// };
+
+// export default ArtPlannerHomePage;
+export default () => {
+  return <>...</>;
+};
