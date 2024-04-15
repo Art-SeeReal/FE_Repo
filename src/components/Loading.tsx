3b5ee3f46ecf63add9c8a7a8125ebcf1eb64c54a
ezyleeso <ezy11am.lsd@gmail.com>
2024-04-15 14:43:03 +0900

    feat: UI 컴포넌트 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { CircleLoader } from 'react-spinners';
+import * as S from './styles';
 
 const Loading = () => {
   return (
-    <>
-      <div>Loading...</div>
+    <S.Row $justifyContent="center">
       <CircleLoader color="#E58AE6" />
-    </>
+    </S.Row>
   );
 };
 
