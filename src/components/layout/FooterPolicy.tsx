76125d3f75ad0a3ad6e90398f0d0f4444a8e727c
ezyleeso <ezy11am.lsd@gmail.com>
2024-04-29 15:30:56 +0900

    feat: 하단 개인정보처리방침 임의 적용

--- src/components/layout/FooterPolicy.tsx
+++ src/components/layout/FooterPolicy.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import * as S from '../styles';
+import { useDialog } from '../../hooks/customs/useDialogState';
+import Dialog from '../Dialog';
+import PrivacyPolicyText from '../policy/PrivacyPolicyText';
 
 const StyledPolicyWrapper = styled.div`
   margin-left: auto;
+
+  ${S.Media.mobile`
+    margin-right: auto;
+  `}
 `;
 
-const FooterPolicy = () => (
-  <StyledPolicyWrapper>
-    <S.Button $style="linkWhite">개인정보처리방침</S.Button>
-  </StyledPolicyWrapper>
-);
+const FooterPolicy = () => {
+  const { openDialog } = useDialog();
+
+  return (
+    <StyledPolicyWrapper>
+      <S.Button
+        $style="linkWhite"
+        onClick={() =>
+          openDialog(
+            <Dialog header="개인정보처리방침" width={960}>
+              <PrivacyPolicyText />
+            </Dialog>,
+          )
+        }
+      >
+        개인정보처리방침
+      </S.Button>
+    </StyledPolicyWrapper>
+  );
+};
 
 export default FooterPolicy;
